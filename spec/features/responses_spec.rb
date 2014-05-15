c5a48386c0c26d54b66ea89c530e8823f3da7e6a
Timmy Huang <TimmyHuang0404@Gmail.com>
2014-05-15 17:04:57 -0400

    Update responses_spec.rb

--- spec/features/responses_spec.rb
+++ spec/features/responses_spec.rb
@@ -3,7 +3,7 @@ describe Response do
   # let!(:rant) { Rant.new(title: "hi", body: "RANTING", responses: [Response.new(body: "comments comments")]) }
   describe "User can view responses of a rant" do
     it "should show comment" do
-      rant = Rant.new(title: "hi", body: "RANTING", responses: [Response.new(body: "comments comments")])
+      rant = Rant.create(title: "hi", body: "RANTING", responses: [Response.create(body: "comments comments")])
       visit rant_path(rant)
       expect(page).to have_content "comments comments"
     end
