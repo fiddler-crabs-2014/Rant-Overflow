a7d849eae3f83de018def1df85d49c29c4022cea
Timmy Huang <timmyhuang0404@gmail.com>
2014-05-15 21:46:21 -0400

    Modify tests to make them pass

--- spec/features/rants_spec.rb
+++ spec/features/rants_spec.rb
@@ -1,9 +1,8 @@
 require "spec_helper"
-describe "Response", js: true do
-  # let!(:rant) { Rant.new(title: "hi", body: "RANTING", responses: [Response.new(body: "comments comments")]) }
+describe Response do
+  let!(:rant) { Rant.create(title: "hi", body: "RANTING", responses: [Response.create(body: "comments comments")]) }
   describe "User can view responses of a rant" do
     it "should show comment" do
-      rant = Rant.create(title: "hi", body: "RANTING", responses: [Response.create(body: "comments comments")])
       visit rant_path(rant)
       expect(page).to have_content "comments comments"
     end
--- spec/features/responses_spec.rb
+++ spec/features/responses_spec.rb
@@ -1,9 +1,8 @@
 require "spec_helper"
-describe "Rants", js: true do
-  # let!(:rant) { Rant.new(title: "hi", body: "RANTING", responses: [Response.new(body: "comments comments")]) }
+describe Response do
+  let!(:rant) { Rant.create(title: "hi", body: "RANTING", responses: [Response.create(body: "comments comments")]) }
   describe "A user" do
     it "should show comment" do
-      rant = Rant.create(title: "hi", body: "RANTING", responses: [Response.create(body: "comments comments")])
       visit rant_path(rant)
       expect(page).to have_content "comments comments"
     end
