757c060b84da7b593fa55ae686c31a7fc782df5a
Timmy Huang <timmyhuang0404@gmail.com>
2014-05-18 19:00:17 -0400

    resolve merge conflict

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,8 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 
+ActiveRecord::Migration.maintain_test_schema!
+
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
